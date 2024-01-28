06a8583158c37d128d5c7f1380eade745b5b0359
genTe <128919388+wdgWon@users.noreply.github.com>
2024-01-29 00:29:00 +0900

    [🐛fix]: 클릭 지점으로 컴포넌트의 좌측 상단이 순간이동되는 문제 해결

--- src/components/Draggable/hook/useDraggable.ts
+++ src/components/Draggable/hook/useDraggable.ts
@@ -1,4 +1,9 @@
-import { useCallback, useEffect, useState } from 'react';
+import {
+  useCallback,
+  useEffect,
+  useState,
+  MouseEvent as ReactMouseEvent,
+} from 'react';
 
 interface Position {
   x: number;
@@ -7,16 +12,23 @@ interface Position {
 
 export const useDraggable = (initPos: Position) => {
   const [pos, setPos] = useState(initPos);
+  const [grabCursorPos, setGrabCursorPos] = useState({ x: 0, y: 0 });
   const [isDragging, setIsDragging] = useState(false);
 
-  const handleMouseDown = useCallback(() => {
+  const handleMouseDown = useCallback((e: ReactMouseEvent) => {
     setIsDragging(true);
+    const rect = e.currentTarget.getBoundingClientRect();
+    const [clientX, clientY] = [e.clientX, e.clientY];
+    setGrabCursorPos({ x: rect.width - clientX, y: rect.height - clientY });
   }, []);
 
   const handleMouseMove = useCallback(
     (e: MouseEvent) => {
       if (!isDragging) return;
-      setPos({ x: e.clientX, y: e.clientY });
+      setPos({
+        x: e.clientX - grabCursorPos.x,
+        y: e.clientY - grabCursorPos.y,
+      });
     },
     [isDragging],
   );
