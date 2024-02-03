6cfcd503565849483109ec9974c34ba082ecf175
dlsxjzld <sinjuk1@naver.com>
2024-02-03 18:23:00 +0900

    [✨feat]: 날씨, 기온, 위치 정보 가져오는 API 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Reset } from 'styled-reset';
 
+import Weather from './components/Weather/Weather';
+
 function App() {
   return (
     <>
       <Reset />
       {/* 컴포넌트 자리 */}
+      <Weather />
     </>
   );
 }
--- src/components/Weather/Weather.tsx
+++ src/components/Weather/Weather.tsx
@@ -1,4 +1,39 @@
+import { useEffect, useState } from 'react';
+
+const API_KEY = 'bc9af38a81220aca0ddd91e15d708947';
+let ignore = false;
+
 const Weather = () => {
+  const [myWeather, setMyWeather] = useState('');
+
+  const getWeatherApiURL = (latitude: number, longitude: number) =>
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
+
+  const onGeoOk = async (position: GeolocationPosition) => {
+    const { latitude, longitude } = position.coords;
+    const URL_END_POINT = getWeatherApiURL(latitude, longitude);
+
+    fetch(URL_END_POINT)
+      .then((res) => res.json())
+      .then((data) => {
+        const { main, name, weather } = data;
+        setMyWeather(weather[0].main);
+        console.log('data', data);
+      });
+  };
+
+  const onGeoError = () => {
+    alert("Can't find you. No weather for you.");
+  };
+
+  useEffect(() => {
+    if (!ignore) {
+      navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+    }
+    return () => {
+      ignore = true;
+    };
+  }, []);
   return <div></div>;
 };
 
